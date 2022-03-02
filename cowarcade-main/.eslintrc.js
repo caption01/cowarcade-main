a0437584a8115f0eb0b76c2ec4aebf146b10c563
Nutchapon Hanouypornlert <nutcha.h@Nutchapons-MacBook-Pro.local>
2022-03-02 22:18:24 +0700

    add tsconfigRootDir config to eslint

---
+++ cowarcade-main/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: ['tsconfig.json', 'next.config.js'],
+    tsconfigRootDir: __dirname
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    semi: 'error',
+    quotes: ['error', 'single', { 'avoidEscape': true }],
+    "jsx-quotes": ["error", "prefer-single"]
+  },
+  env: {
+    browser: true,
+    es2021: true
+  },
+  ignorePatterns: ['next.config.js', '.eslintrc.js']
+};
