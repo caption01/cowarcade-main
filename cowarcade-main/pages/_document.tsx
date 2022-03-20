f59fa9fa5379a381348b26914ee2d24f2392c9ad
Nutchapon Hanouypornlert <nutcha.h@Nutchapons-MacBook-Pro.local>
2022-03-20 19:32:18 +0700

    setup carousel

--- cowarcade-main/pages/_document.tsx
+++ cowarcade-main/pages/_document.tsx
@@ -66,6 +66,17 @@ export default class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap'
             rel='stylesheet'
           />
+          <link
+            rel='stylesheet'
+            type='text/css'
+            charset='UTF-8'
+            href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css'
+          />
+          <link
+            rel='stylesheet'
+            type='text/css'
+            href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css'
+          />
         </Head>
         <body>
           <Main />
--- cowarcade-main/pages/index.tsx
+++ cowarcade-main/pages/index.tsx
@@ -1,8 +1,24 @@
+import { useState } from 'react';
 import { NextPage } from 'next';
 import Link from 'next/link';
 import { map } from 'lodash';
+import Slider from 'react-slick';
+import styled from 'styled-components';
 
 import { HomePageLayout, Text, Box, Tag } from 'src/components';
+
+const Content = styled.div`
+  width: 50vw;
+  height: 300px;
+  background: red;
+  margin: auto;
+`;
+
+const Card = styled.div`
+  height: 250px;
+  background: blue;
+  border: 2px green solid;
+`;
 
 const tags = [
   {
@@ -27,24 +43,80 @@ const tags = [
   },
 ];
 
+const cards = [
+  {
+    itemIndex: 0,
+    title: 0,
+    status: (itemIndex, activeIndex) =>
+      itemIndex === activeIndex ? 'item-active' : 'non-active',
+  },
+  {
+    itemIndex: 1,
+    title: 1,
+    status: (itemIndex, activeIndex) =>
+      itemIndex === activeIndex ? 'item-active' : 'non-active',
+  },
+  {
+    itemIndex: 2,
+    title: 2,
+    status: (itemIndex, activeIndex) =>
+      itemIndex === activeIndex ? 'item-active' : 'non-active',
+  },
+];
+
+const settings = {
+  className: 'center',
+  centerMode: true,
+  infinite: true,
+  centerPadding: '200px',
+  slidesToShow: 1,
+  speed: 500,
+};
+
 const Home: NextPage = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  function handleBeforeChange(currentSlide: number, nextSlide: number): void {
+    setActiveIndex(nextSlide);
+  }
+
   return (
     <HomePageLayout>
-      <Text type='header'>
-        🙋‍♂️ I'm a "Developer" with a passionate for Cloud ☁️, Linux 📟, and Web
-        technology 🖥.
-      </Text>
-      <Box>
-        {map(tags, (tag) => (
-          <Tag margin={[1.2, 0]} padding={0.4} mr={2} color={tag?.color}>
-            <Link href={tag?.url} passHref>
-              <a target={'_blank'} style={{ color: 'inherit' }}>
-                {tag?.title}
-              </a>
-            </Link>
-          </Tag>
-        ))}
+      <Box width={'60vw'}>
+        <Text type='header'>
+          🙋‍♂️ Hi My name is Nutchapon, I'm a "Developer" with a passionate for
+          Cloud ☁️, Linux 📟, and Web technology 🖥.
+        </Text>
+        <Box margin={[2, 0]}>
+          {map(tags, (tag) => (
+            <Tag
+              key={tag?.title}
+              margin={[1.2, 0]}
+              padding={0.4}
+              mr={2}
+              color={tag?.color}
+            >
+              <Link href={tag?.url} passHref>
+                <a target={'_blank'} style={{ color: 'inherit' }}>
+                  {tag?.title}
+                </a>
+              </Link>
+            </Tag>
+          ))}
+        </Box>
       </Box>
+      <Content>
+        <div>
+          <Slider beforeChange={handleBeforeChange} {...settings}>
+            {map(cards, (c, idx) => (
+              <Card key={c?.itemIndex}>
+                <h3>{c?.title}</h3>
+                <h3>{c?.status(c?.itemIndex, activeIndex)}</h3>
+              </Card>
+            ))}
+          </Slider>
+        </div>
+      </Content>
     </HomePageLayout>
   );
 };
